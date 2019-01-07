8247c4747e3d9cc80ed308d25f95f75defcb3d16
sheridanstewart <sastew@stanford.edu>
2019-01-06 23:54:52 -0600

    Update find_similar.py

--- find_similar.py
+++ find_similar.py
@@ -9,24 +9,25 @@ from sklearn.metrics.pairwise import cosine_similarity
 try:
 	assert len(sys.argv) >= 4
 except:
-	print("Usage: python find_similar.py directory subreddit subreddit...subreddit num_matches")
+	print("Usage: python find_similar.py directory pickleflag subreddits num_matches")
 	
 dir_ = sys.argv[1]
-subreddits = sys.argv[2:-1]
+pickleFlag = sys.argv[2]
+subreddits = sys.argv[3:-1]
 n = int(sys.argv[-1])
 
-subredditlist = pickle.load(open("subredditlist.p", "rb"))
+subredditlist = pickle.load(open("{}_subredditlist.p".format(pickleFlag), "rb"))
 
 if n >= len(subredditlist):
 	n = len(subredditlist) - 1
 	
-user_counts = pickle.load(open("user_counts.p", "rb"))
+user_counts = pickle.load(open("{}_user_counts.p".format(pickleFlag), "rb"))
 assert len(subredditlist) == len(user_counts)
 
-tfidf_matrix_raw = pickle.load(open("tfidf_matrix_raw.p", "rb"))
+tfidf_matrix_raw = pickle.load(open("{}_tfidf_matrix_raw.p".format(pickleFlag), "rb"))
 assert len(subredditlist) == tfidf_matrix_raw.shape[0]
 
-tfidf_matrix_logged = pickle.load(open("tfidf_matrix_logged.p", "rb"))
+tfidf_matrix_logged = pickle.load(open("{}_tfidf_matrix_logged.p".format(pickleFlag), "rb"))
 assert len(subredditlist) == tf_idf_matrix_logged.shape[0]
 
 
