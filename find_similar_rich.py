da69f050be8dc6f752506265b091187d546e3280
sheridanstewart <sastew@stanford.edu>
2019-01-07 00:31:16 -0600

    Update find_similar_rich.py

--- find_similar_rich.py
+++ find_similar_rich.py
@@ -1,4 +1,5 @@
 import pickle
+import numpy as np
 import sys
 from prettytable import PrettyTable
 from sklearn.feature_extraction.text import CountVectorizer
@@ -13,15 +14,18 @@ except:
 	
 dir_ = sys.argv[1]
 pickleFlag = sys.argv[2]
-subreddits = sys.argv[3:-2]
-n = int(sys.argv[-2])
-method = sys.argv[-1]
+subreddits = sys.argv[3:-3]
+n = int(sys.argv[-3])
+method = sys.argv[-2]
+log = sys.argv[-1]
 
 try:
 	assert method in ["average", "max"]
+	assert log in ["t", "true", "f", "false"]
 except:
-	print("Usage: python find_similar_rich.py directory pickleflag subreddits num_matches method")
+	print("Usage: python find_similar_rich.py directory pickleflag subreddits num_matches method logtruefalse")
 	print("Methods: average, max")
+	print("Log: true or false")
 
 subredditlist = pickle.load(open("{}_subredditlist.p".format(pickleFlag), "rb"))
 
@@ -31,44 +35,41 @@ if n >= len(subredditlist):
 user_counts = pickle.load(open("{}_user_counts.p".format(pickleFlag), "rb"))
 assert len(subredditlist) == len(user_counts)
 
-tfidf_matrix_raw = pickle.load(open("{}_tfidf_matrix_raw.p".format(pickleFlag), "rb"))
-assert len(subredditlist) == tfidf_matrix_raw.shape[0]
+if not log:
+	user_matrix = pickle.load(open("{}_tfidf_matrix_raw_users.p".format(pickleFlag), "rb"))
+	content_matrix = pickle.load(open("{}_tfidf_matrix_raw_content.p".format(pickleFlag), "rb"))
+	assert len(subredditlist) == tfidf_matrix_raw.shape[0]
+else:
+	user_matrix = pickle.load(open("{}_tfidf_matrix_logged_users.p".format(pickleFlag), "rb"))
+	content_matrix = pickle.load(open("{}_tfidf_matrix_logged_content.p".format(pickleFlag), "rb"))
+	assert len(subredditlist) == tf_idf_matrix_logged.shape[0]
 
-tfidf_matrix_logged = pickle.load(open("{}_tfidf_matrix_logged.p".format(pickleFlag), "rb"))
-assert len(subredditlist) == tf_idf_matrix_logged.shape[0]
 
-
-def find_similar(matrix, index, top_n):
+def find_similar(matrix1, matrix2, index, top_n):
 	#sims = linear_kernel(matrix[index:index+1], matrix).flatten() # relies on l2 norm; import commented out
-	sims = cosine_similarity(matrix[index:index+1], matrix).flatten() # no normalization needed
+	sims1 = cosine_similarity(matrix1[index:index+1], matrix1).flatten() # no normalization needed
+	sims2 = cosine_similarity(matrix2[index:index+1], matrix2).flatten()
+	if method == "max":
+		sims = np.maximum(sims1, sims2)
+	else:
+		sims = (sims1 + sims2)/2
 	related_docs_indices = [i for i in sims.argsort()[::-1] if i != index]
 	return [(index, sims[index]) for index in related_docs_indices][0:top_n]
 
 
 for subreddit in subreddits:
-	cols = ["Top {} Matches for {} with Raw TFs:".format(n, subreddit), "Cosine:", "Users:"]
-	cols = cols + ["With Logged TFs:", "Cosine:", "Users:"]
-	matches_raw = []
-	scores_raw = []
-	counts_raw = []
-	matches_logged = []
-	scores_logged = []
-	counts_logged = []
+	cols = ["Top {} Matches for {}:".format(n, subreddit), "Cosine:", "Users:"]
+	matches = []
+	scores = []
+	counts = []
 	table = PrettyTable()
 	subreddit_index = subredditlist.index(subreddit)
-	for index, score in find_similar(tfidf_matrix_raw, subreddit_index, n):
-		matches_raw.append("{}".format(subredditlist[index]))
-		scores_raw.append("{:.2f}".format(score))
-		counts_raw.append(user_counts[index])
-	for index, score in find_similar(tfidf_matrix_logged, subreddit_index, n):
-		matches_logged.append("{}".format(subredditlist[index]))
-		scores_logged.append("{:.2f}".format(score))
-		counts_logged.append()
-	table.add_column(cols[0], matches_raw)
-	table.add_column(cols[1], scores_raw)
-	table.add_column(cols[2], counts_raw)
-	table.add_column(cols[3], matches_logged)
-	table.add_column(cols[4], scores_logged)
-	table.add_column(cols[5], counts_logged)
+	for index, score in find_similar_rich(user_matrix, content_matrix, subreddit_index, n):
+		matches.append(subreddlist[index])
+		scores.append(score)
+		counts.append(user_counts[index])
+	table.add_column(cols[0], matches)
+	table.add_column(cols[1], scores)
+	table.add_column(cols[2], counts)
 	print(table)
 	print("")
