ba0cda9455a10da519e3b1b29d55ca0a306b394b
sheridanstewart <sastew@stanford.edu>
2019-01-06 23:46:17 -0600

    Update merge_submissions.py

--- merge_submissions.py
+++ merge_submissions.py
@@ -7,13 +7,13 @@ firstMonth = int(sys.argv[3])
 lastMonth = int(sys.argv[4])
 
 for year in range(firstYear, lastYear + 1):
-  for month in range(firstMonth, lastMonth + 1):
-    with open("RS_{}-{}.tsv".format(year, month), "r") as reader:
-      for line in reader:
-        line = line.split("\t")
-        subreddit = line[2]
-        submission = re.sub("[^a-z0-9]", " ", line[7].lower())
-        submission = re.sub("\s+", " ", submission)
-        fname = "{}-{}-{}-{}-{}.txt".format(subreddit, firstYear, lastYear, firstMonth, lastMonth)
-        with open(fname, "a+") as writer:
-          writer.write(" {} ".format(submission)
+	for month in range(firstMonth, lastMonth + 1):
+		with open("RS_{}-{}.tsv".format(year, month), "r") as reader:
+			for line in reader:
+				line = line.split("\t")
+				subreddit = line[2]
+				submission = re.sub("[^a-z0-9]", " ", line[7].lower())
+				submission = re.sub("\s+", " ", submission)
+				fname = "{}-{}-{}-{}-{}.txt".format(subreddit, firstYear, lastYear, firstMonth, lastMonth)
+				with open(fname, "a+") as writer:
+					writer.write(" {} ".format(submission)
