c9b9839bd7d706d8e54e32713df49273fb444652
sheridanstewart <sastew@stanford.edu>
2019-01-06 18:53:41 -0600

    Update find_similar.py

--- find_similar.py
+++ find_similar.py
@@ -7,20 +7,31 @@ try:
 	assert len(sys.argv) >= 4
 except:
 	print("Usage: python find_similar.py directory subreddit subreddit...subreddit num_matches")
-
-tfidf_matrix_logged = pickle.load(open("tfidf_matrix_logged.p", "rb"))
-tfidf_matrix_raw = pickle.load(open("tfidf_matrix_raw.p", "rb"))
-
-def find_similar(matrix, index, top_n):
-	sims = linear_kernel(matrix[index:index+1], matrix).fatten()
-	related_docs_indices = [i for i in sims.argsort()[::-1] if i != index]
-	return [(index, sims[index]) for index in related_docs_indices][0:top_n]
 	
 dir_ = sys.argv[1]
 subreddits = sys.argv[2:-1]
 n = int(sys.argv[-1])
 
-all = [tup[0] for tup in tfidf
+subredditlist = pickle.load(open("subredditlist.p", "rb"))
+
+if n >= len(subredditlist):
+	n = len(subredditlist) - 1
+	
+tfidf_matrix_logged = pickle.load(open("tfidf_matrix_logged.p", "rb"))
+tfidf_matrix_raw = pickle.load(open("tfidf_matrix_raw.p", "rb"))
+
+
+def find_similar(matrix, index, top_n):
+	sims = linear_kernel(matrix[index:index+1], matrix).fatten()
+	related_docs_indices = [i for i in sims.argsort()[::-1] if i != index]
+	return [(index, sims[index]) for index in related_docs_indices][0:top_n]
+
 
 for subreddit in subreddits:
+	cols = ["Top {} Matches for {}:".format(n, subreddit), "With Raw TFs:", "Cosine:", "Num. Unique Users:", "With Logged TFs:", "Cosine:", "Num. Unique Users:"]
+	matches_raw = []
+	scores_raw = []
+	matches_logged = []
+	scores_logged = []
+	t = PrettyTable()
 	
