6f8c7cf7336e5019850a8c6c84c16bf0ac6b4fc7
sheridanstewart <sastew@stanford.edu>
2019-01-06 23:52:39 -0600

    Update make_tfidf_matrices.py

--- make_tfidf_matrices.py
+++ make_tfidf_matrices.py
@@ -1,14 +1,17 @@
 import glob
 import pickle
 import sys
-from sklearn.feature_extractin.text import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 corpus = []
 subredditList = []
 
 dir_ = sys.argv[1]
-for file in glob.glob("{}/*combined.txt".format(dir_)):
+fnameFormatting = sys.argv[2]
+pickleFlag = sys.argv[3]
+
+for file in glob.glob("{}/*{}.txt".format(dir_, fnameFormatting)):
 	with open(file, "r") as reader:
 		subreddit = file.replace("{}/".format(dir_), "").replace("_combined.txt", "")
 		corpus.append((subreddit, reader.read()))
@@ -18,9 +21,9 @@ cv = CountVectorizer()
 tdm = cv.fit_transform([content for file, content in corpus])
 
 user_counts = tdm.toarray().astype(bool).sum(axis=1)
-del cv, tdm
-
 assert len(subredditList) == len(user_counts)
+
+del cv, tdm
 
 # NON-NORMALIZED -- will not work with sklearn.metrics.pairwise.linear_kernel()
 tf_raw = TfidfVectorizer(norm=None, analyzer="word", ngram_range=(1,1), stop_words=["[deleted]"], sublinear_tf=False)
@@ -32,7 +35,7 @@ tf_logged = TfidfVectorizer(norm=None, analyzer="word", ngram_range=(1,1), stop_
 tfidf_matrix_logged = tf_logged.fit_transform([content for file, content in corpus])
 assert len(subredditList) == tfidf_matrix_logged.shape[0]
 
-pickle.dump(subredditList, open("subredditlist.p", "wb"))
-pickle.dump(user_counts, open("user_counts.p", "wb"))
-pickle.dump(tfidf_matrix_logged, open("tfidf_matrix_logged.p", "wb"))
-pickle.dump(tfidf_matrix_raw, open("tfidf_matrix_raw.p", "wb"))
+pickle.dump(subredditList, open("{}_subredditlist.p".format(pickleFlag), "wb"))
+pickle.dump(user_counts, open("{}_user_counts.p".format(pickleFlag), "wb"))
+pickle.dump(tfidf_matrix_raw, open("{}_tfidf_matrix_raw.p".format(pickleFlag), "wb"))
+pickle.dump(tfidf_matrix_logged, open("{}_tfidf_matrix_logged.p".format(pickleFlag), "wb"))
