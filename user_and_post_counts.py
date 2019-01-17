039b325c88f0207b568b034c48f204705281108c
sheridanstewart <sastew@stanford.edu>
2019-01-17 11:15:16 -0800

    Add files via upload

---
+++ user_and_post_counts.py
@@ -0,0 +1,45 @@
+import os
+import pickle
+import sys
+from collections import defaultdict
+
+readDir = sys.argv[1]
+
+post_counts = defaultdict(lambda: 0)
+user_counts = defaultdict(lambda: set())
+
+for f in os.listdir(readDir):
+	if f[-4:] == ".tsv":
+		if f[:2] == "RS":
+			with open(f, "r") as reader:
+				for line in reader:
+					line = line.split("\t")
+					sub = line[2]
+					post_counts[sub] += 1
+		elif f[:2] == "RC":
+			with open(f, "r") as reader:
+				users = set()
+				for line in reader:
+					line = line.split("\t")
+					sub = line[2]
+					user = line[3]
+					user_counts[sub].add(user)
+
+pickle.dump(post_counts, open("post_counts.p", "wb"))
+pickle.dump(user_counts, open("user_counts.p", "wb"))
+
+post_counts = [(key, post_counts[key]) for key in user_counts.keys()]
+post_counts.sort(key=lambda x: x[1], reverse=True)
+with open("post_counts.txt", "a") as writer:
+	for tup in post_counts:
+		writer.write("{},\t{}\n".format(tup[0], tup[1]))
+
+user_counts = [(key, len(user_counts[key])) for key in user_counts.keys()]
+user_counts.sort(key=lambda x: x[1], reverse=True)
+with open("user_counts.txt", "a") as writer:
+	for tup in user_counts:
+		writer.write("{},\t{}\n".format(tup[0], tup[1]))
+
+print(len(user_counts))
+print(len(post_counts))
+assert len(user_counts) == len(post_counts)
